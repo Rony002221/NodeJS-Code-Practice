ac03875a4849ceeb234827b46fcc00d3e9ff17a9
rony <rony49883@gmail.com>
2021-05-19 22:37:43 +0600

    edit the file and add simple route logic

--- request_response/main.js
+++ request_response/main.js
@@ -2,7 +2,25 @@ var http = require('http');
 
 var server =  http.createServer(function (req, res){
 
-    res.end("Hello world");
+    if(req.url == "/")
+    {
+        res.writeHead(200,{"content-type": "text/html"})
+        res.write('<h1><u> This is Home Page </u></h1>')
+        res.end();
+    }
+    else if(req.url == "/profile")
+    {
+        res.writeHead(200,{"content-type": "text/html"})
+        res.write('<h1><u> This is Profile Page </u></h1>')
+        res.end();
+    }
+
+    else if(req.url == "/contact")
+    {
+        res.writeHead(200,{"content-type": "text/html"})
+        res.write('<h1><u> This is Contact Page </u></h1>')
+        res.end();
+    }
 });
 
 server.listen(4000);
